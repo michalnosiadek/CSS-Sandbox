f57f04dca18bdfe6e1e11b024cf2f0d8cc4ba8b1
mi.nosiadek <mi.nosiadek@ecol.com.pl>
2023-06-21 13:08:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function openNavLeft() {
+    document.getElementById("mySidebarLeft").style.width = "450px";
+    document.getElementById("main").style.marginLeft = "450px";
+  }
+  
+  function closeNavLeft() {
+    document.getElementById("mySidebarLeft").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+  function openNavRight() {
+    document.getElementById("mySidebarRight").style.width = "450px";
+    document.getElementById("main").style.marginRight = "450px";
+  }
+  
+  function closeNavRight() {
+    document.getElementById("mySidebarRight").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+  }
